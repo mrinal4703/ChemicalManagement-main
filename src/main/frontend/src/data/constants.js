af8017d3e7e3864ab7f58a6c255869a95d4d621c
mrinal4703 <mrinal.gupta@mitaoe.ac.in>
2024-11-10 17:00:31 +0530

    elb dns

--- src/main/frontend/src/data/constants.js
+++ src/main/frontend/src/data/constants.js
@@ -6,4 +6,4 @@ export const email_session = sessionStorage.getItem('loggedinuseremail') || '';
 
 export const rank = localStorage.getItem('loggedinuserrank') || '';
 export const rank_session = sessionStorage.getItem('loggedinuserrank') || '';
-export const username = 'localhost';
+export const username = 'Seemsan-alb-1250430078.ap-south-1.elb.amazonaws.com';
