6762c774141054cf47a13ddb93a07e9efc337c43
mrinal4703 <mrinal.gupta@mitaoe.ac.in>
2024-11-10 21:39:50 +0530

    alb username

--- src/main/frontend/src/data/constants.js
+++ src/main/frontend/src/data/constants.js
@@ -3,7 +3,6 @@ export const isLoggedIn = localStorage.getItem('isLoggedIn') || false;
 export const isLoggedIn_session = sessionStorage.getItem('isLoggedIn') || false;
 export const email = localStorage.getItem('loggedinuseremail') || '';
 export const email_session = sessionStorage.getItem('loggedinuseremail') || '';
-
 export const rank = localStorage.getItem('loggedinuserrank') || '';
 export const rank_session = sessionStorage.getItem('loggedinuserrank') || '';
 export const username = 'Seemsan-alb-209984179.ap-south-1.elb.amazonaws.com';
